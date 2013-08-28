2849d2c707a8fdf3abe5acb793d5a4cc069ab03f
Shido DArkland <sdarkland@localhost.localdomain>
2013-08-28 11:55:20 -0600

    Agregado de la clase Main.

--- practica1/Main.cs
+++ practica1/Main.cs
@@ -3,10 +3,11 @@ using System;
 namespace practica1
 {
 	class MainClass
-	{
+	{	
+			
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Hello World!\n How you doing?");
 		}
 	}
 }
